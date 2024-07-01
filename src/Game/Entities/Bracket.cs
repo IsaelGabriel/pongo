1ee083c7bd8c24938538ac863a1fb8b7d3cfc0fa
IsaelGabriel <isaelgsfaria@gmail.com>
2024-07-01 16:59:28 -0300

    Add player input

--- src/Game/Entities/Bracket.cs
+++ src/Game/Entities/Bracket.cs
@@ -3,6 +3,7 @@ using Raylib_cs;
 
 public class Bracket(Vector2 position) : IEntity {
 
+    public const float BracketSpeed = 150f;
     public const int BracketWidth = 8;
     public const int BracketHeight = Global.WindowHeight / 5;
     public Rectangle rect = new(position, new(BracketWidth, BracketHeight));
--- src/Game/Scenes/MainScene.cs
+++ src/Game/Scenes/MainScene.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Raylib_cs;
 
 public class MainScene : IEntity {
@@ -7,6 +8,17 @@ public class MainScene : IEntity {
         new(new(16, (Global.WindowHeight - Bracket.BracketHeight) / 2)),
         new(new(Global.WindowWidth - 16 - Bracket.BracketWidth, (Global.WindowHeight - Bracket.BracketHeight) / 2)),
     ];
+
+    public void Update() {
+        HandlePlayerInput();
+    }
+
+    private void HandlePlayerInput() {
+        float diffY = (float) (Raylib.IsKeyDown(KeyboardKey.Down) - Raylib.IsKeyDown(KeyboardKey.Up));
+        Vector2 newPosition = brackets[0].position;
+        newPosition.Y += diffY * Bracket.BracketSpeed * Raylib.GetFrameTime();
+        brackets[0].position = newPosition;
+    }
 
     public void Render() {
         foreach(Bracket bracket in brackets) {
