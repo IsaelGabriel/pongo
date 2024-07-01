646566eeeee1c0dd24ec616e2764cfa12be9079b
IsaelGabriel <isaelgsfaria@gmail.com>
2024-07-01 16:37:44 -0300

    Add base for Game loop

---
+++ src/Game/Global.cs
@@ -0,0 +1,5 @@
+public static class Global {
+    public const int WindowWidth = 600;
+    public const int WindowHeight = 400;
+    public const int TargetFPS = 60;
+}
--- src/Game/Program.cs
+++ src/Game/Program.cs
@@ -1,1 +1,21 @@
 ﻿
+using Raylib_cs;
+
+public static class Game {
+
+    public static bool ShouldClose = false;
+
+    static void Main() {
+        Raylib.InitWindow(Global.WindowWidth, Global.WindowHeight, "Pongo");
+
+        Raylib.SetTargetFPS(Global.TargetFPS);
+
+        while(!ShouldClose) {
+
+            
+            if(Raylib.WindowShouldClose()) ShouldClose = true;
+        }
+
+        Raylib.CloseWindow();
+    }
+}
